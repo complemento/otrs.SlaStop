dc6f4545ed5bd8267e2ef8dc5fb6e35522bfd61d
rmustapha <rabi@complemento.net.br>
2017-09-27 11:32:04 -0300

    Realizadas modificações no javascript para que o código também seja aplicado na interface de Customer.

--- var/httpd/htdocs/js/Complemento.TicketEscalation.js
+++ var/httpd/htdocs/js/Complemento.TicketEscalation.js
@@ -14,11 +14,23 @@ Core.TE.TicketEscalation = (function (TargetNS) {
     // container and content for ticket escalation time (yes, it's a fixed time value)
     var $EscTimeP = $('fieldset.TableLike div.Value p'),
         $EscTimeDiv = $('td.EscalationTimeEntry div'),
+        $EscTimeSpan = $('span.CustomST'),
         regexEscTimeStopped = new RegExp('(29.12.2025|12.30.2025|2025.12.30|12\/30\/2025|30\/12\/2025) [0-9][0-9]:00');
 
     // replacing in ticket zoom mask
     if ($EscTimeP.length) {
         $EscTimeP.each( function(){
+            // check if ticket escalation time is stopped
+            if (regexEscTimeStopped.test($(this).text())) {
+                // reformat ticket escalation time into text
+                $(this).text('(SLA SUSPENSO)').attr('title', 'Escalação Suspensa devido ao Estado do Chamado');
+            }
+        });
+    }
+    
+    // replacing in customer ticket zoom mask
+     if ($EscTimeSpan.length) {
+        $EscTimeSpan.each( function(){
             // check if ticket escalation time is stopped
             if (regexEscTimeStopped.test($(this).text())) {
                 // reformat ticket escalation time into text
